f02e135cbca605e0d2b4f6a8b2a521cd440ae99a
vijay <vijayavenkatesh.vn@gmail.com>
2020-10-02 10:40:53 +0530

    ReverseString initial

---
+++ src/test/java/ReverseString.java
@@ -0,0 +1,10 @@
+public class ReverseString {
+
+    public static void main(String[] args){
+
+        String str = "raj kumar";
+        for(int i=str.length()-1; i>=0; i--){
+            System.out.print(str.charAt(i));
+        }
+    }
+}
