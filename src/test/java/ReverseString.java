2c0d9b6a249f2d9c22d2089ba4397b17c6ae8662
vijay <vijayavenkatesh.vn@gmail.com>
2020-10-02 16:13:25 +0530

    concatenated string

--- src/test/java/ReverseString.java
+++ src/test/java/ReverseString.java
@@ -2,7 +2,7 @@ public class ReverseString {
 
     public static void main(String[] args){
 
-        String str = "raj kumar";
+        String str = "raj kumar venkat prabhu";
         for(int i=str.length()-1; i>=0; i--){
             System.out.print(str.charAt(i));
         }
