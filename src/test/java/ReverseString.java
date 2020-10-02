dffdc1eaa1963223eb6cc4fcc3c8e0b3aceee915
Vijayavenkatesh <33842130+vijavenka@users.noreply.github.com>
2020-10-02 16:10:17 +0530

    Update ReverseString.java
    String array

--- src/test/java/ReverseString.java
+++ src/test/java/ReverseString.java
@@ -3,8 +3,12 @@ public class ReverseString {
     public static void main(String[] args){
 
         String str = "raj kumar";
-        for(int i=str.length()-1; i>=0; i--){
-            System.out.print(str.charAt(i));
+        String[] str1 = str.split(" ");
+        
+        for(int j=str1.length; j>0; j--){
+        for(int i=str[j].length()-1; i>=0; i--){
+            System.out.print(str[j].charAt(i));
+        }
         }
     }
 }
