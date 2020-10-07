9a5ec71d903cded914b0a123fd88b7fb1d1ef5a3
vijay <vijayavenkatesh.vn@gmail.com>
2020-10-07 14:48:35 +0530

    updated as per review

--- src/test/java/CountObjects.java
+++ src/test/java/CountObjects.java
@@ -14,7 +14,7 @@ public class CountObjects {
     public static void main(String[] args){
         Students student1 = new Students("Vijay", 32);
         Students student4 = new Students("Shan", 19);
-        Students student5 = new Students("Varun Dhan", 49);
+        Students student5 = new Students("Varun Dhawan", 49);
         Students student6 = new Students("Vanitha Nagaraj", 23);
 
         System.out.println("Total count of Objects " + Students.countObject);
